67aaf0ab55c9086a563485021db17b45b3e91026
sunilrajramchandani <sunilraj.ramchandani@benilde.edu.ph>
2017-01-19 17:53:00 +0800

    new welcome page w bs

--- register.php
+++ register.php
@@ -1,3 +1,17 @@
+<?php
+	# write your comment here
+    // write your other comment here
+	/*
+		write your
+		comment here
+	*/
+	$studentNo = "";
+	$lastName = "";
+	$firstName = "";
+	$emailAddress = "";
+	$password = "";
+	$birthDate = null;
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,7 +23,7 @@
 			<h1 class="text-center">Student Registration</h1>
 			<div class="col-lg-offset-3 col-lg-6">
 				<div class="well">
-					<form method="GET" action="welcome.php"
+					<form method="POST" action="welcome.php"
 						class="form-horizontal">
 						<div class="form-group">
 							<label class="control-label col-lg-4">
@@ -65,7 +79,14 @@
 									class="form-control" required />
 							</div>
 						</div>
-
+						<div class="form-group">
+							<div class="col-lg-offset-4 col-lg-8">
+								<button name="register"
+									class="btn btn-success">
+									Register
+								</button>
+							</div>
+						</div>
 					</form>
 				</div>
 			</div>
