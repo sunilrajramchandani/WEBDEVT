582865ec337b9946437ffcf4d08dfb54fee6d752
sunilrajramchandani <sunilraj.ramchandani@benilde.edu.ph>
2017-01-21 20:05:55 +0800

    registration final

--- register.php
+++ register.php
@@ -10,87 +10,84 @@
 	$firstName = "";
 	$emailAddress = "";
 	$password = "";
-	$birthDate = null;
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Student Registration</title>
-		<link href="css/button.css" rel="stylesheet" />
+		<link href="css/registration.css" rel="stylesheet" />
 		<link href="css/bootstrap.min.css" rel="stylesheet" />
 		
 	</head>
 	<body>
 		<div class="container">
 			<h1 class="text-center">Student Registration</h1>
+			<hr>
 			<div class="col-lg-3">
 			</div>
 			<div class="col-lg-6">
-				<div class="well">
-					<form method="POST" action="welcome.php"
+					<form name = "test" method="POST" action="welcome.php"
 						class="form-horizontal">
 						<div class="form-group">
-							<label class="control-label col-lg-4">
-								Student ID
-							</label>
-							<div class="col-lg-8">
+							<div class="col-lg-offset-1 col-lg-5">
 								<input type="text" name="sid"
-									class="form-control" required />
+									class="form-control input-lg" placeholder="Student ID" required />
 							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-lg-4">
-								Last Name
-							</label>
-							<div class="col-lg-8">
+							<div class="col-lg-5">
 								<input type="text" name="ln"
-									class="form-control" required />
+									class="form-control input-lg" placeholder="Last Name" required />
 							</div>
 						</div>
+						
 						<div class="form-group">
-							<label class="control-label col-lg-4">
-								First Name
-							</label>
-							<div class="col-lg-8">
+							<div class="col-lg-offset-1  col-lg-5">
 								<input type="text" name="fn"
-									class="form-control" required />
+									class="form-control input-lg" placeholder="First Name" required />
 							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-lg-4">
-								Email Address
-							</label>
-							<div class="col-lg-8">
-								<input type="email" name="email"
-									class="form-control" required />
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-lg-4">
-								Password
-							</label>
-							<div class="col-lg-8">
+							<div class="col-lg-5">
 								<input type="password" name="pwd"
-									class="form-control" required />
+									class="form-control input-lg" placeholder = "User Name" required />
 							</div>
+							
 						</div>
+						
 						<div class="form-group">
-							<label class="control-label col-lg-4">
-								Birthdate
-							</label>
-							<div class="col-lg-8">
-								<input type="date" name="bday"
-									class="form-control" required />
+							<div class="col-lg-offset-1 col-lg-5">
+								<input type="password" name="pwd" id = "password"
+									class="form-control input-lg" placeholder = "Set a Password" required />
+							</div>
+							<div class="col-lg-5">
+								<input type="text" name="cpwd" id = "password2"
+									class="form-control input-lg" placeholder="Confirm Password" onchange="match()" required />
+							</div>
+							<script>
+						function match(){
+							if (test.password2.value == test.password.value) {
+							window.alert("matched");
+							}
+							if (test.password2.value != test.password.value) {
+							window.alert("not matched");
+							}
+						}
+					</script>
+							
+						</div>
+						
+						
+						
+						<div class="form-group">
+							<div class="col-lg-offset-1 col-lg-10">
+								<input type="text" name="email"
+									class="form-control input-lg" placeholder="Email Address" required />
 							</div>
 						</div>
 						<div class="form-group">
 
-							<div class="col-lg-offset-3 col-lg-4">
-								<button name="register" class="btn style2">
+							<div class="col-lg-offset-5 col-lg-7">
+								<button name="register" class="myButton btn-lg">
 									<span>Register</span>
 								</button>
 							</div>
-						</div>
 						</div>
 					</form>
 				</div>
